11deba22fd2f48a9cedebaa68860a875bcebdc92
phatdroid94 <phatdroid94@gmail.com>
2021-01-16 18:42:12 +0700

    finish

--- app/src/main/java/com/example/intentexplicit24112020/MainActivity.java
+++ app/src/main/java/com/example/intentexplicit24112020/MainActivity.java
@@ -7,17 +7,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void sendString(View view) {
         Intent intent = new Intent(MainActivity.this,MainActivity2.class);
-        intent.putExtra("chuoi","Hello");
+        intent.putExtra(AppConstant.KEY_CHUOI,"Hello");
+        startActivity(intent);
+    }
+
+    public void sendIntArray(View view) {
+        int[] arrNums = {1,2,3,4,5,6,7,8,9,10};
+        Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+        intent.putExtra(AppConstant.KEY_MANG_SO,arrNums);
+        startActivity(intent);
+    }
+
+    public void sendObject(View view) {
+        Student student = new Student("Nguyen Van Teo",10);
+        Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+        intent.putExtra(AppConstant.KEY_OBJECT, student);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/intentexplicit24112020/MainActivity2.java
+++ app/src/main/java/com/example/intentexplicit24112020/MainActivity2.java
@@ -13,10 +13,22 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         // 1 : Nhan du lieu dang string
+//        Intent intent = getIntent();
+//        if (intent != null){
+//            String chuoi = intent.getStringExtra(AppConstant.KEY_CHUOI);
+//            Toast.makeText(this, chuoi, Toast.LENGTH_SHORT).show();
+//        }
+        // 2 : Nhan du lieu dang mang so
+//        Intent intent = getIntent();
+//        if (intent != null){
+//            int[] arrNums = intent.getIntArrayExtra(AppConstant.KEY_MANG_SO);
+//            Toast.makeText(this, arrNums.length + "", Toast.LENGTH_SHORT).show();
+//        }
+        // 3 : Nhan du lieu dang object
         Intent intent = getIntent();
         if (intent != null){
-            String chuoi = intent.getStringExtra("chuoi");
-            Toast.makeText(this, chuoi, Toast.LENGTH_SHORT).show();
+            Student student = (Student) intent.getParcelableExtra(AppConstant.KEY_OBJECT);
+            Toast.makeText(this, student.name + "", Toast.LENGTH_SHORT).show();
         }
     }
 }
